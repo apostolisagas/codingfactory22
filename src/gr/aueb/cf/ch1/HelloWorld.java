e4dd36f752f24b41b80f6d3ace1ec35fcacd0e6a
apostolisagas <apostolosagas@gmail.com>
2023-05-31 15:14:33 +0300

    CF22-v0.1

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,37 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Εκτυπώνει Hello World! στο
+ * standard output (κονσόλα).
+ *
+ * @author agas
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints Hello World
+
+        /*
+        *
+        */
+        System.out.println("Hello World!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/gr/aueb/cf/ch3/StarsApp.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.ch3;
+
+import java.util.Scanner;
+
+/**
+ * Εκτυπώνει n οριζόντια αστεράκια,
+ * όπου n ένας ακέραιοσ που δίνει ο
+ * χρήστης
+ */
+public class StarsApp {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfStars = 0;
+        int i = 1;
+
+        System.out.println("Please insert number of stars");
+        numberOfStars = in.nextInt();
+
+
+        while (i <= numberOfStars) {
+            System.out.print("*");
+            i++;
+            }
+        }
+    }
+
