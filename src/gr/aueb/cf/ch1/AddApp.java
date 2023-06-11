027e3413b739310787173fd4a84aa1c889d67988
apostolisagas <apostolosagas@gmail.com>
2023-06-11 19:45:48 +0300

    v1.2
    git commit -m v1.2
    ls -la

--- src/gr/aueb/cf/ch1/AddApp.java
+++ src/gr/aueb/cf/ch1/AddApp.java
@@ -1,21 +1,30 @@
 package gr.aueb.cf.ch1;
 
-/** Προσθέτει δύο ακεραίους
- * και εμφανίζει το αποτέλεσμα
- * στο stout
+/** Adds two integers.
+ *
+ * @author agas
  */
 public class AddApp {
     public static void main(String[] args) {
-        //Δήλωση και αρχικοποίηση μεταβλητών
+
         int num1 = 10;
         int num2 = 7;
         int result = 0;
 
-        //Εντολές
+
         result = num1 + num2;
 
-        // Εκτύπωση αποτελεσμάτων}
+
         System.out.println("Το αποτέλεσμα των " + num1 + " + " + num2 + " είναι: " + result);
         System.out.printf("Το αποτέλεσμα των %d , %d είναι %d", num1 , num2, result);
     }
+
+    /**
+     * Adds two ints.
+     *
+     * @param a     the first int.
+     * @param b     the second int.
+     * @return      the sum of a, b.
+     */
+    public static int add(int a, int b) {return a + b; }
 }
--- src/gr/aueb/cf/ch1/HelloWorld.java
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -1,18 +1,26 @@
 package gr.aueb.cf.ch1;
 
+import gr.aueb.cf.ch1.model.Point;
+import static java.lang.Math.abs;
+import java.util.Scanner;
+
+
 /**
- * Εκτυπώνει Hello World! στο
- * standard output (κονσόλα).
+ * Prints <b>Hello World!</b>
+ * Defines a {@link HelloWorld} type.
  *
  * @author agas
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Prints Hello World
+ * @version 1.0
+ * @since 0.1
+ */public class HelloWorld {
 
-        /*
-        *
-        */
+     private Scanner sc = new Scanner(System.in);
+     private Point point;
+     private String s;
+
+    public static void main(String[] args) {
+
+        int abs = abs(-3);
         System.out.println("Hello World!");
     }
 }
